2be91a1eb46ccec610e9aaf82b3b9d8688e62a77
Anda Lixandroiu <lix.anda@yahoo.com>
2016-01-06 01:28:15 +0200

    Add Note object

---
+++ Notes/app/src/main/java/data/Note.java
@@ -0,0 +1,35 @@
+package data;
+
+/**
+ * Created by anda on 1/6/2016.
+ */
+public class Note {
+
+    public String _title;
+    public String _content;
+    public String _date;
+
+    public String get_title() {
+        return _title;
+    }
+
+    public void set_title(String _title) {
+        this._title = _title;
+    }
+
+    public String get_content() {
+        return _content;
+    }
+
+    public void set_content(String _content) {
+        this._content = _content;
+    }
+
+    public String get_date() {
+        return _date;
+    }
+
+    public void set_date(String _date) {
+        this._date = _date;
+    }
+}
