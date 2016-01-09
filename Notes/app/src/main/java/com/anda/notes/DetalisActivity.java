9cd1e116ceb267419270b41fe9f2578f9746dd0f
Anda Lixandroiu <lix.anda@yahoo.com>
2016-01-09 03:31:29 +0200

    Insert 2 new buttons in activity_main and make that when user click on the title of  the note, he can see note deatils like content and date

--- Notes/app/src/main/java/com/anda/notes/DetalisActivity.java
+++ Notes/app/src/main/java/com/anda/notes/DetalisActivity.java
@@ -1,39 +1,36 @@
 package com.anda.notes;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class DetalisActivity extends ActionBarActivity {
+
+    private TextView _titleText;
+    private TextView _contentText;
+    private TextView _dateText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detalis);
-    }
+        _titleText = (TextView)findViewById(R.id.title);
+        _contentText = (TextView)findViewById(R.id.theNote);
+        _dateText = (TextView)findViewById(R.id.theDate);
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_detalis, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if(bundle!=null) {
+            String title = (String) bundle.get("title");
+            String content = (String) bundle.get("content");
+            String date = (String) bundle.get("date");
+            _titleText.setText(title);
+            _contentText.setText(content);
+            _dateText.setText(date);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 }
